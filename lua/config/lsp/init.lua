b9267e07ba0f8b7b8a68896b6294d6bfb44641bf
lucasyarza <lucasyarza@gmail.com>
2022-04-13 19:55:32 +0200

    LSP server config

--- lua/config/lsp/init.lua
+++ lua/config/lsp/init.lua
@@ -3,21 +3,5 @@ if not status_ok then
   return
 end
 
-local status_ok_lsp_installer, lsp_installer = pcall(require, "nvim-lsp-installer")
-if not status_ok_lsp_installer then
-  return
-end
-
-lsp_installer.on_server_ready(function(server)
-    local opts = {}
-
-    -- (optional) Customize the options passed to the server
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
-
-    -- This setup() function will take the provided server configuration and decorate it with the necessary properties
-    -- before passing it onwards to lspconfig.
-    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    server:setup(opts)
-end)
+require "config.lsp.lsp-installer"
+require("config.lsp.handlers").setup()
